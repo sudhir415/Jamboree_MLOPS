89bbb6dfd7a090c8382a565d2de313fb3061dc18
Sudhir Malik <sudhirmalik415@gmail.com>
2024-01-24 02:03:02 +0530

    Data Validation done

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.JambooreProject import logger 
 from src.JambooreProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.JambooreProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -12,3 +13,16 @@ if __name__ == '__main__':
     except Exception as e:
         logger.exception(e)
         raise e 
+    
+
+STAGE_NAME = "Data Validation stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataValidationTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
--- src/JambooreProject/config/configuration.py
+++ src/JambooreProject/config/configuration.py
@@ -1,6 +1,7 @@
 from src.JambooreProject.constants import *
 from src.JambooreProject.utils.common import create_directories, read_yaml
 from src.JambooreProject.entity.config_entity import  DataIngestionConfig
+from src.JambooreProject.entity.config_entity import DataValidationConfig
 
 class ConfigurationManager:
     def __init__(
@@ -29,4 +30,19 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir 
         )
 
-        return data_ingestion_config
+        return data_ingestion_config 
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+        root_dir=config.root_dir,
+        STATUS_FILE=config.STATUS_FILE,
+        unzip_data_dir = config.unzip_data_dir,
+        all_schema=schema,
+        )
+
+        return data_validation_config
