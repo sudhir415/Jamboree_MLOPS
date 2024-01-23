8ab365d12dd605668f0972774692996fd129f50e
Sudhir Malik <sudhirmalik415@gmail.com>
2024-01-24 00:15:16 +0530

    Data Ingestion Step done

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.JambooreProject import logger 
+from src.JambooreProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Hello World")
+STAGE_NAME = "Data Ingestion stage"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e 
